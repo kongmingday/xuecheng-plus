4dcec2b8c91d3de3f0670684dad1e426df50d8e2
Err0r <11407917+err0r_000674@user.noreply.gitee.com>
2023-03-01 20:12:12 +0800

    teachplan manager finish

---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/xuecheng_plus/mapper/CourseCategoryMapper.java
@@ -0,0 +1,16 @@
+package com.xuecheng.xuecheng_plus.mapper;
+
+import com.xuecheng.xuecheng_plus.model.po.CourseCategory;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <p>
+ * 课程分类 Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ */
+public interface CourseCategoryMapper extends BaseMapper<CourseCategory> {
+
+}
---
+++ xuecheng-plus-generator/src/main/java/com/xuecheng/xuecheng_plus/service/CourseCategoryService.java
@@ -0,0 +1,16 @@
+package com.xuecheng.xuecheng_plus.service;
+
+import com.xuecheng.xuecheng_plus.model.po.CourseCategory;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 课程分类 服务类
+ * </p>
+ *
+ * @author itcast
+ * @since 2023-01-26
+ */
+public interface CourseCategoryService extends IService<CourseCategory> {
+
+}
